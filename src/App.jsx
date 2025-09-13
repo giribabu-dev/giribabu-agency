66299c8e0bee00c3bbf8a2ada3c6ef3304c89615
giribabu-a <giribabuannapareddi@gmail.com>
2025-09-13 13:34:08 +0530

    Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Navbar from "./components/Navbar";
+import { useState } from 'react';
+
 function App() {
+
+  const [theme, setTheme] = useState('light')
+
   return (
-    <div>
-      App Component
+    <div className="darK:bg-black relative">
+      <Navbar theme={theme} setTheme={setTheme} />
     </div>
   )
 }
